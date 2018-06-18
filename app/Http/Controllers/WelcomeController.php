fdd64fb42bc02e3d8526b3df6a4c69e59c0055aa
jiro.kirameki <ec2-user@ip-172-31-41-166.us-east-2.compute.internal>
2018-06-18 04:07:21 +0000

    test

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -17,9 +17,6 @@ class WelcomeController extends Controller
      */
     public function index()
     {
-        $items = Item::orderBy('updated_at', 'desc')->paginate(20);
-        return view('welcome', [
-            'items' => $items,
-        ]);
+        echo test
     }
 }
